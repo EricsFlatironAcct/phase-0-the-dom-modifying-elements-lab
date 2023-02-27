1fcd05d99efd6dc5656ae16b0dab31382df6bc40
Eric Krukowski <erickruk@amazon.com>
2023-02-27 01:06:52 -0500

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainMain = document.getElementById("main");
+mainMain.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Eric is the champion";
+document.body.append(newHeader);
